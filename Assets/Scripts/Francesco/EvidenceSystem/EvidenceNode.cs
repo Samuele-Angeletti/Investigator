e115df69a4fcb74170ddcdb36892b00ec78fbc9f
felipc273 <felipc273@gmail.com>
2026-06-19 15:14:52 +0200

    EvidenceSystem first version completed

---
+++ Assets/Scripts/Francesco/EvidenceSystem/EvidenceNode.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EvidenceSystem/" + nameof(EvidenceNode))]
+public class EvidenceNode : ScriptableObject
+{
+    public int Id;
+    [TextArea]
+    public string Description;
+    public GameObject EvidenceModel;
+    public EEvidenceType EvidenceType;
+    [Range(0f,1f)]
+    public float TruthValue;
+    public GameObject LinkedSuspect;
+    public List<EvidenceTag> EvidenceTags;
+}
