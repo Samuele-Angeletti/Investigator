cf54ab47d4deb6694062f625c0c6eb8ebb11279b
felipc273 <felipc273@gmail.com>
2026-06-23 15:42:05 +0200

    Add evidence model

---
+++ Assets/Scripts/Francesco/EvidenceSystem/EvidenceModel.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EvidenceModel : MonoBehaviour
+{
+    [SerializeField] private EvidenceNode _evidenceNode;
+    public EvidenceNode EvidenceNode
+    {
+        get
+        {
+            return _evidenceNode;
+        }
+        set
+        {
+            _evidenceNode = value;
+        }
+    }
+
+    [SerializeField, Range(0f, 1f)]
+    private float _visibility = 0f;
+    private bool _isCollected;
+    public bool IsCollected => _isCollected;
+
+    public void AddVisibility(float visibility)
+    {
+        _visibility += visibility;
+        _visibility = Mathf.Clamp01(_visibility);
+    }
+
+    public void Initialize(EvidenceNode evidenceNode)
+    {
+        _evidenceNode = evidenceNode;
+        _isCollected = false;
+        _visibility = 0f;
+    }
+}
