04958c00187711375fa7e7d6db553941ae67bd23
felipc273 <felipc273@gmail.com>
2026-07-01 17:28:13 +0200

    Started UI for journal

---
+++ Assets/Scripts/Francesco/EvidenceSystem/Journal.cs
@@ -0,0 +1,21 @@
+using DesignPatterns.Generics;
+using UnityEngine;
+
+public class Journal : Singleton<Journal>
+{
+    public override void Awake()
+    {
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
