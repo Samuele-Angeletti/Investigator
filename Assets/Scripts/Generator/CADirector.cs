a4e7f202e725769479ac2268976615b1ed596a6c
xanakoby <scottmoreno6@gmail.com>
2026-07-04 14:48:57 +0200

    CA_Update
    espansa un pochino la base del CA

--- Assets/Scripts/Generator/CADirector.cs
+++ Assets/Scripts/Generator/CADirector.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CADirector : MonoBehaviour
 {
+    GenerationResult generationResult;
+
     [Header("Cellular Automata Settings")]
     [SerializeField] int caWidth = 50;
     [SerializeField] int caHeight = 50;
@@ -10,6 +12,41 @@ public class CADirector : MonoBehaviour
     [SerializeField] int caBirthLimit = 4;
     [SerializeField] int caDeathLimit = 3;
     [SerializeField] bool caSolidBorder = true;
-    [SerializeField] int caSeed = 45;
-    [SerializeField] bool caRandomSeed = false;
+
+    [ContextMenu("Debug_BSP")]
+    public void GenerateBSPDebug(Vector2Int _area)
+    {
+        generationResult = GenerateCellularAutomata(_area);
+    }
+    private GenerationResult GenerateCellularAutomata()
+    {
+        return new CellularAutomataGenerator(new CellularAutomataSettings()
+        {
+            Width = caWidth,
+            Height = caHeight,
+            InitialWallChance = caInitialWallChance,
+            Steps = caSteps,
+            BirthLimit = caBirthLimit,
+            DeathLimit = caDeathLimit,
+            Seed = 0,
+            SolidBorder = caSolidBorder,
+            RandomSeed = true
+        }).Generate();
+    }
+
+    private GenerationResult GenerateCellularAutomata(Vector2Int _area)
+    {
+        return new CellularAutomataGenerator(new CellularAutomataSettings()
+        {
+            Width = _area.x,
+            Height = _area.y,
+            InitialWallChance = caInitialWallChance,
+            Steps = caSteps,
+            BirthLimit = caBirthLimit,
+            DeathLimit = caDeathLimit,
+            Seed = 0,
+            SolidBorder = caSolidBorder,
+            RandomSeed = true
+        }).Generate();
+    }
 }
